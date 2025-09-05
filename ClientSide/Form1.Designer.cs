8f3e2d8d9802baf7c611020bc31c68ab621a3e8f
NENEPE <oleksandrprystavsky@gmail.com>
2025-09-05 14:19:33 +0200

    Added and designed an interface for Client Side

--- ClientSide/Form1.Designer.cs
+++ ClientSide/Form1.Designer.cs
@@ -1,4 +1,10 @@
-﻿namespace ClientSide
+﻿using System.Drawing.Printing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+using Font = System.Drawing.Font;
+
+namespace ClientSide
 {
     partial class Form1
     {
@@ -28,12 +34,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(316, 117);
+            textBox1.Margin = new Padding(4, 3, 4, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(221, 22);
+            textBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Agency FB", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(36, 111);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(259, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Enter Ip Address to connect to the server:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Agency FB", 48F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(72, 22);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(426, 79);
+            label1.TabIndex = 3;
+            label1.Text = "Currency Converter";
+            // 
+            // button1
+            // 
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Agency FB", 18F);
+            button1.Location = new Point(72, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(192, 42);
+            button1.TabIndex = 6;
+            button1.Text = "Connect";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Enabled = false;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Agency FB", 18F);
+            button2.Location = new Point(304, 159);
+            button2.Name = "button2";
+            button2.Size = new Size(192, 42);
+            button2.TabIndex = 7;
+            button2.Text = "Disconnect";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(118, 233);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(99, 23);
+            comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(345, 233);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(99, 23);
+            comboBox2.TabIndex = 9;
+            // 
+            // button3
+            // 
+            button3.Enabled = false;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Agency FB", 18F);
+            button3.Location = new Point(186, 262);
+            button3.Name = "button3";
+            button3.Size = new Size(192, 42);
+            button3.TabIndex = 10;
+            button3.Text = "Convert";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(4F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(587, 331);
+            Controls.Add(button3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Agency FB", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(2, 3, 2, 3);
+            MaximizeBox = false;
+            Name = "Form1";
+            ShowIcon = false;
+            Text = "Converter";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Label label2;
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Button button3;
     }
 }
