6f669521bdf4cc1c8a807396aea033d09ca74520
NENEPE <oleksandrprystavsky@gmail.com>
2025-09-05 14:12:13 +0200

    Added "Currency" class library

---
+++ Currency/CurrencyData.cs
@@ -0,0 +1,17 @@
+﻿namespace Currency
+{
+    [Serializable]
+    public enum CurrencyName { UAH, USD, EUR, PLN }
+    [Serializable]
+    public class CurrencyClient
+    {
+        public CurrencyName? c1;
+        public CurrencyName? c2;
+        public string user;
+    }
+    [Serializable]
+    public class CurrencyServer
+    {
+        public string res;
+    }
+}
